4ab5fed4ee0080b9313ffd93ce77f49d146aca45
LilRev2024 <revids14@gmail.com>
2023-06-07 12:26:11 -0500

    Initial commit

---
+++ questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " Which of the following statements about living cells is false?",
+    answer: "They are found in all animals but not in all plants",
+    options: [
+      "Most are microscopic",
+      "They are found in all animals but not in all plants",
+      "They are the smallest basic units that can carry out all of the functions that we normally define as life.",
+      "They are basic building blocks of all living things"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Chromosomes are found in ___________ of cells.?",
+    answer: "the nucleus",
+    options: [
+      "the nucleus",
+      "the cytoplasm",
+      "both the nucleus and the cytoplasm",
+      "center"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following statements is true about the chromosomes of different plant and animal species?",
+    answer: "They may differ in number, shape, and size.",
+    options: [
+      "They may differ in number, but are the same shape and size.",
+      "They may differ in the shape and size, but normally have the same number.",
+      "They may differ in number, shape, and size.",
+      "The are all the same"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following statements is true about cells?",
+    answer: "The nucleus is within the nuclear membrane which is surrounded by the cytoplasm.",
+    options: [
+      "The nucleus is within the cell membrane which is surrounded by the nuclear membrane.",
+      "The nucleus is within the nuclear membrane which is surrounded by the cytoplasm.",
+      "The cytoplasm is within the nuclear membrane.",
+      "Cells only carry out one function"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The sex chromosomes of normal females are:",
+    answer: "X and X",
+    options: [
+      "X and Y",
+      "Y and Y",
+      "X and X",
+      "none of the above"
+    ]
+  },
+
+];
